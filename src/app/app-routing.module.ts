75386eb3bc041c1efd94654f3c604647f8c3f7e4
ivan <ibodnar@outlook.com>
2018-12-22 15:27:28 -0300

    Routing para ""

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {SearchComponent} from './components/search/search.component';
+
+const routes: Routes = [
+  { path: '', component: SearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
