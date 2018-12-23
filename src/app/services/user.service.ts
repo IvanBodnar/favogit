b7b1da73f6af8f42fc312cbefa31ddfdf201dee2
ivan <ibodnar@outlook.com>
2018-12-23 00:25:46 -0300

    UserService.fetchUsers guarda el resultado de dataService.getUsers en un subject

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import {DataService} from './data.service';
-import {Observable, of} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private _usersSubject = new Subject();
+  users$ = this._usersSubject.asObservable();
 
   constructor(
     private dataService: DataService
   ) { }
 
-  fetchUsers( userName: string ): Observable<any> {
-    return this.dataService.getUsers( userName );
+  fetchUsers( userName: string ): void {
+    this.dataService.getUsers( userName )
+      .subscribe(
+        users => {
+          this._usersSubject.next(users);
+        }
+      );
   }
 }
