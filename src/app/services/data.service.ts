6e86edbb3dc020c7038728e6e8cb184456b74b1a
ivan <ibodnar@outlook.com>
2018-12-23 09:13:07 -0300

    Datos para auth

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,25 +3,37 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {UsersResponseModel} from '../models/users-response.model';
 import {map} from 'rxjs/operators';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  baseUrl = `https://api.github.com/`;
+  headers = {
+    'Accept': 'application/vnd.github.v3.text-match+json',
+    'Authorization': `token ${ environment.token }`
+  };
 
   constructor(
     private http: HttpClient
   ) { }
 
-  private static _constructUrl( username: string ): string {
-    return `https://api.github.com/search/users?q=${ username }+in:login+in:fullname+in:email+type:user&per_page=20`;
+  private _constructUrl( username: string ): string {
+    return `
+      ${ this.baseUrl }search/users?q=${ username }+in:login+in:fullname+in:email+type:user&per_page=20
+    `;
   }
 
   init(): void {}
 
+  // Trae la lista de users que coinciden con la búsqueda y mapea
+  // la response a UsersResponseModel. Los headers que agrega hacen
+  // que también venga el campo text_matches con los detalles del
+  // match (cuál fue el o los campos matcheados)
   getUsers( userName: string ): Observable<UsersResponseModel> {
-    return this.http.get<any>( DataService._constructUrl( userName ),
-      { headers: { 'Accept': 'application/vnd.github.v3.text-match+json' } }
+    return this.http.get<any>( this._constructUrl( userName ),
+      { headers: this.headers }
       )
       .pipe(
         map(
