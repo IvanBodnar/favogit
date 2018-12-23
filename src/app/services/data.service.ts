646aaee03a010a5f722d22ee365ad1b811f29cca
ivan <ibodnar@outlook.com>
2018-12-22 21:49:02 -0300

    Agregado parámetro per_page a url en DataService._constructUrl

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,8 +11,8 @@ export class DataService {
     private http: HttpClient
   ) { }
 
-  private static _constructUrl(username: string): string {
-    return `https://api.github.com/search/users?q=${ username }+in:login+in:fullname+in:email+type:user`;
+  private static _constructUrl( username: string ): string {
+    return `https://api.github.com/search/users?q=${ username }+in:login+in:fullname+in:email+type:user&per_page=20`;
   }
 
   init(): void {}
