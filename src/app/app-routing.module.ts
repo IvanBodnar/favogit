fa98a9a7de9a1baad41a98732e66e083ecff4865
ivan <ibodnar@outlook.com>
2018-12-23 16:52:27 -0300

    Se agrega route para detail-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {UserListComponent} from './components/user-list/user-list.component';
+import {UserDetailComponent} from './components/user-detail/user-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: 'users', component: UserListComponent }
+  { path: 'users', component: UserListComponent },
+  { path: 'user-detail', component: UserDetailComponent },
 ];
 
 @NgModule({
