c00cf8f8131848090863430c6eaba81d5ce09e4d
ivan <ibodnar@outlook.com>
2018-12-24 16:32:35 -0300

    MessageComponent -> Se agrega un setter para messageText, para mostrar el msj cuando se pasa el texto a mostrar. Se agregan métodos showMessage y hideMessage

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -6,11 +6,25 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
-  @Input() messageText: string;
+  private _messageText: string;
+  @Input()
+  set messageText( message: string ) {
+    this._messageText = message;
+    if (message) { this.showMessage(); }
+  }
+  opacity = { 'opacity': 0 };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showMessage(): void {
+    this.opacity = { 'opacity': 0.9 };
+  }
+
+  hideMessage(): void {
+    this.opacity = { 'opacity': 0 };
   }
 
 }
