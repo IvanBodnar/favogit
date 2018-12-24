5672ba2037322987c772cf26575d081a6da9bbd5
ivan <ibodnar@outlook.com>
2018-12-24 10:21:30 -0300

    Se pasa el array de objetos del service al component en favorite-list

--- src/app/components/favorite-list/favorite-list.component.ts
+++ src/app/components/favorite-list/favorite-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import {FavoriteService} from '../../services/favorite.service';
+
 @Component({
   selector: 'app-favorite-list',
   templateUrl: './favorite-list.component.html',
   styleUrls: ['./favorite-list.component.css']
 })
 export class FavoriteListComponent implements OnInit {
+  favoritesArray: any[];
 
-  constructor() { }
+  constructor(
+    public favoriteService: FavoriteService
+  ) { }
 
   ngOnInit() {
+    this.favoritesArray = this.favoriteService.favorites;
   }
 
 }
